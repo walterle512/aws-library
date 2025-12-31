1b37865ce9b857d6f50f5e80d0559c6a98991e1b
Long Vu <longvu@MacBook-Pro-cua-Long.local>
2025-12-31 21:47:12 +0700

    Initial commit

---
+++ test/nestjs-appsync-lambda-books.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestjsAppsyncLambdaBooks from '../lib/nestjs-appsync-lambda-books-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nestjs-appsync-lambda-books-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestjsAppsyncLambdaBooks.NestjsAppsyncLambdaBooksStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
